0822a9166abad41d941ca946551cc5764cc83f71
Linux User <new@beaglebone.(none)>
2000-01-01 01:15:34 +0000

    add lots of files
    Hacking with everyone.

--- deploy/home/new/scrape_sms.py
+++ deploy/home/new/scrape_sms.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python
+import sys
 import cmgpy
+import time
+
 at = cmgpy.commands.at
 icon322 = cmgpy.Modem('/dev/ttyHS0', 115200)
 
@@ -7,16 +10,17 @@ sms_file = open('sms_history.txt', 'a') # append all SMS to file
 
 result = icon322.AT(at.cmgl(stat="ALL"))
 messages = result.messageList
+print "found messages: %s" % (len(messages))
 for message in messages:
+    print message
     # {'date': '13/02/23,13:05:54-24', 'cmgl': '1', 'stat': 'REC READ', 'message': 'Str: blather!', 'da': '+19082165058'}
-    formatted_message_string = message['date'] + ";" + 
-                               message['da'] + ";\"" +
-                               message['message'] + "\""
-    formatted_message_string = formatted_message_string.replace('\n','^n')
-    formatted_message_string = formatted_message_string.replace('\r','^r')
+    formatted_message_string = message['date'] + ";" + message['da'] + ";\"" + message['message'] + "\""
+    formatted_message_string = formatted_message_string.strip( )
+    formatted_message_string = formatted_message_string.strip( )
+
     sms_file.write(formatted_message_string+'\n')
 
-
-# could be moved before loop
 icon322.AT(at.cmgd(index=0, flag=4)) # delete ALL SMS with flag=4
 
+#####
+# EOF
