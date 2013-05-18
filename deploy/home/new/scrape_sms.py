be1aa42c697960a572730fcfa541ef47a4499393
Nathan West <nate.ewest@gmail.com>
2013-05-18 13:20:16 -0400

    changes to scrape_sms.py that work on beagle, added base64 dependency

--- deploy/home/new/scrape_sms.py
+++ deploy/home/new/scrape_sms.py
@@ -1,14 +1,19 @@
+import base64
 import cmgpy
-at = pymodem.commands.at
-icon322 = pymodem.Modem('/dev/ttyHS0', 115200)
+at = cmgpy.commands.at
+icon322 = cmgpy.Modem('/dev/ttyHS0', 115200)
 
 sms_file = open('sms_history.txt', 'a') # append all SMS to file
 
 result = icon322.AT(at.cmgl(stat="ALL"))
 messages = result.messageList
 for message in messages:
-    # do we format the SMS first? 
-    # TODO: add an example of what gets printed here
-    sms_file.write(message)
-    icon322.AT(at.cmgd(index=0, flag=4)) # delete ALL SMS with flag=4
+    # {'date': '13/02/23,13:05:54-24', 'cmgl': '1', 'stat': 'REC READ', 'message': 'Str: blather!', 'da': '+19082165058'}
+    formatted_message_string = message['date'] + ";" + 
+                               message['da'] + ";\"" +
+                               message['message'] + "\""
+    sms_file.write(base64.encodestring(message))
+
+# could be moved before loop
+icon322.AT(at.cmgd(index=0, flag=4)) # delete ALL SMS with flag=4
 
