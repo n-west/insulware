124213d7e48faf68a8d3e778a1e068cadf534718
root <root@beaglebone.(none)>
2013-05-22 06:38:52 +0000

    more

--- deploy/home/new/continuous_read_sms.sh
+++ deploy/home/new/continuous_read_sms.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 export PATH=.:$PATH
+echo "modemsetup"
 ./modemsetup.sh
-./stream_modem.sh | ./getsmsid.sh | while read sms_id ; do
+echo "streaming modem"
+#( rewind_messages ;
+#( ./stream_modem.sh | ./getsmsid.sh )) | while read sms_id ; do
+( ./stream_modem.sh | ./getsmsid.sh ) | while read sms_id ; do
   sms=$(get_sms_at_index.sh $sms_id | grep STR)
-  echo $(date +%FT%T): "$sms"
-  read preamble config_url <<<$sms
-  # config_url=$(echo "$sms" | cut -d' ' -f 2)
+  echo "$(date +%FT%T): ${sms_id}: $sms"
+  config_url=$(echo "$sms" | cut -d' ' -f 2)
   echo $(date +%FT%T): config_url "$config_url"
-  test -n ${config_url} && ( rm new.config ; validate_config "${config_url}" new.config )
+  rm -f new.config 
+  test -n ${config_url} && validate_config "${config_url}" new.config
   if [[ -f new.config ]] ; then
     mv new.config good.config
     # do the auditing
-    echo $(date +%FT%T): auditing..."
-    tail -n 40 good.config
+    echo "$(date +%FT%T): auditing..."
     setup_session good.config
     # delete $sms_id
-    echo $(date +%FT%T): deleting $sms_id"
-    delete_sms_at $sms_id
+    echo "$(date +%FT%T): deleting ${sms_id}"
+    # delete_sms_at $sms_id
   fi
 done
--- deploy/home/new/get_sms_at_index.sh
+++ deploy/home/new/get_sms_at_index.sh
@@ -1,1 +1,1 @@
-(echo "AT+CMGF=1"; echo "AT+CMGR=${1}"; echo ;   ) | socat - /dev/ttyHS0,raw,echo=0,crlf,nonblock
+(echo "AT+CMGF=1"; echo "AT+CMGR=${1}"; sleep 1 ; echo ; sleep 1;  ) | socat - /dev/ttyHS1,raw,echo=0,crlf,nonblock
